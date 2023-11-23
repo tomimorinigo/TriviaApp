2c577e34e770b397b4c9c23fca37b5b4dba684d7
tomiimorinigo <morinigotomas1@gmail.com>
2023-11-23 12:59:58 -0300

    Added connection to the database and request for questions to it.

---
+++ src/main/java/trivia/ConectorSQL.java
@@ -0,0 +1,11 @@
+package trivia;
+
+public class ConectorSQL {
+    // JDBC driver name and database URL
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	public static final String DB_URL = "jdbc:mysql://localhost:3306/tienda";
+	
+	// Database credentials
+	public static final String USER = "root";
+	public static final String PASS = "admin";
+}
